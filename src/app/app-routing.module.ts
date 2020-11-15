514198fc876629d3cc25ca71eeb70129ccba0cd9
karolan1 <karolan1@fit.cvut.cz>
2020-11-15 20:46:51 +0100

    app init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {InruptComponent} from './inrupt/inrupt.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inrupt', component: InruptComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InruptComponent } from './inrupt/inrupt.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InruptComponent
   ],
   imports: [
     BrowserModule,
