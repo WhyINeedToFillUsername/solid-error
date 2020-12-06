dafae113daf8820d7526ef06bb8cfac7f9ad6372
karolan1 <karolan1@fit.cvut.cz>
2020-12-06 22:26:48 +0100

    using ngx-build-plus for custom webpack config + force webpack 5 (needs yarn instead of npm)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+declare const VERSION: string;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'solid-error';
+  title = 'Version: ' + VERSION;
 }
